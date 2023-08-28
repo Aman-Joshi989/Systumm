d095fd6ca65f16c8774b48fba070269f14aa0e15
AmanJoshi <AmanJoshi254@gmail.com>
2023-08-28 13:09:08 +0530

    Initial commit

--- video_subtitles_project/subtitles_app/models.py
+++ video_subtitles_project/subtitles_app/models.py
@@ -1,15 +1,10 @@
-
 from django.db import models
 
 class Video(models.Model):
     title = models.CharField(max_length=100)
     video_file = models.FileField(upload_to='videos/')
-
+    
 class Subtitle(models.Model):
-    video = models.OneToOneField(Video, on_delete=models.CASCADE, primary_key=True)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    timestamp = models.FloatField()
     text = models.TextField()
-    timestamp = models.DecimalField(max_digits=10, decimal_places=3)
-
-    def __str__(self):
-        return f"Subtitle for {self.video.title} - {self.timestamp}"
-
